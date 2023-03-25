9db9554f1773764aa4a3a5a7a947a2ce383eb0ec
Bahadır Aksakal <72493865+bahadraksakal@users.noreply.github.com>
2023-03-25 18:52:10 +0300

    bootstrapt sorunu çözüldü

--- Bulus-Tanis-Frontend/src/components/UserDash.vue
+++ Bulus-Tanis-Frontend/src/components/UserDash.vue
@@ -8,30 +8,27 @@
 
   <div class="row  g-0 z bg-danger">
     <div class="col-3 all-user shadow-lg p-3">
-	
-	 <div >
-            <button type="button" data-toggle="modal" class="btn btn-primary konumara-btn" data-target="#modalsearch" > Konum Ara</button>
+      <div>
+	<button type="button" class="btn btn-primary konumara-btn" data-bs-toggle="modal" data-bs-target="#modalsearch" >Konum Ara</button>
       </div>
 
-<div class="modal" id="modalsearch" tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Save changes</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+	<div class="modal fade" id="modalsearch" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+		<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+		<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+	      </div>
+	      <div class="modal-body">
+		...
+	      </div>
+	      <div class="modal-footer">
+		<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+		<button type="button" class="btn btn-primary">Save changes</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
  
       <div class="text-center mb-2 p-0"><h3>Chat List</h3></div>
       <all-user
